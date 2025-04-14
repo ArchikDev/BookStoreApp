cafdd9c54ca05fb3d711ce8e13fa017a604d9904
a.galeev <a.galeev@malltech.tu>
2025-04-14 14:57:24 +0500

    Initial commit

---
+++ app/src/main/java/ru/ar4uk/bookstoreapp/MainActivity.kt
@@ -0,0 +1,110 @@
+package ru.ar4uk.bookstoreapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import ru.ar4uk.bookstoreapp.data.Book
+import ru.ar4uk.bookstoreapp.ui.theme.BookStoreAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            BookStoreAppTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val fs = Firebase.firestore
+    val list = remember {
+        mutableStateOf(emptyList<Book>())
+    }
+
+    val listener = fs.collection("books")
+        .addSnapshotListener { snapShow, exception ->
+            list.value = snapShow?.toObjects(Book::class.java) ?: emptyList()
+        }
+
+//    listener.remove() // удалить при смене экрана
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(.8f)
+        ) {
+            items(list.value) { book ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                ) {
+                    Text(
+                        text = book.name,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth()
+                            .padding(15.dp)
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            onClick = {
+            fs.collection("books")
+                .document()
+                .set(
+                    Book(
+                        name = "My book",
+                        description = "sdfsf",
+                        price = "100",
+                        category = "fiction",
+                        imageUrl = "url"
+                    )
+                )
+        }) {
+            Text(
+                text = "Add Book"
+            )
+        }
+    }
+}
---
+++ app/src/main/java/ru/ar4uk/bookstoreapp/data/Book.kt
@@ -0,0 +1,9 @@
+package ru.ar4uk.bookstoreapp.data
+
+data class Book(
+    val name: String = "",
+    val description: String = "",
+    val price: String = "",
+    val category: String = "",
+    val imageUrl: String = ""
+)
