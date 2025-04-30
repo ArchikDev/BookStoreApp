fec22240cae40ede05eda17a85404d6ebd4c44b2
a.galeev <a.galeev@malltech.tu>
2025-04-30 22:14:45 +0500

    refactor

---
+++ app/src/main/java/ru/ar4uk/bookstoreapp/di/MainModule.kt
@@ -0,0 +1,42 @@
+package ru.ar4uk.bookstoreapp.di
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ru.ar4uk.bookstoreapp.utils.firebase.FirebaseManager
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return Firebase.auth
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFireStore(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseManager(
+        auth: FirebaseAuth,
+        db: FirebaseFirestore
+    ): FirebaseManager {
+        return FirebaseManager(auth, db)
+    }
+
+
+
+}
---
+++ app/src/main/java/ru/ar4uk/bookstoreapp/utils/firebase/FirebaseManager.kt
@@ -0,0 +1,132 @@
+package ru.ar4uk.bookstoreapp.utils.firebase
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FirebaseFirestore
+import ru.ar4uk.bookstoreapp.data.Book
+import ru.ar4uk.bookstoreapp.data.Favorite
+import javax.inject.Singleton
+
+@Singleton
+class FirebaseManager(
+    private val auth: FirebaseAuth,
+    private val db: FirebaseFirestore
+) {
+    private fun getAllFavsIds(
+        onFavs: (List<String>) -> Unit
+    ) {
+        getFavsCategoryReference()
+            .get()
+            .addOnSuccessListener { task ->
+                val idsList = task.toObjects(Favorite::class.java)
+                val keysList = arrayListOf<String>()
+                idsList.forEach {
+                    keysList.add(it.id)
+                }
+                onFavs(keysList)
+            }
+            .addOnFailureListener {  }
+    }
+
+    private fun getAllFavsBooks(
+        onBooks: (List<Book>) -> Unit
+    ) {
+        getAllFavsIds { idsList->
+            if (idsList.isNotEmpty()) {
+                db.collection("books")
+                    .whereIn(FieldPath.documentId(), idsList)
+                    .get()
+                    .addOnSuccessListener { task ->
+                        val booksList = task.toObjects(Book::class.java).map {
+                            if (idsList.contains(it.id)) {
+                                it.copy(isFavorite = true)
+                            } else {
+                                it
+                            }
+                        }
+
+                        onBooks(booksList)
+                    }
+                    .addOnFailureListener {  }
+            } else {
+                onBooks(emptyList())
+            }
+        }
+    }
+
+    private fun getAllBooks(
+        onBooks: (List<Book>) -> Unit
+    ) {
+        getAllFavsIds { idsList->
+            if (idsList.isNotEmpty()) {
+                db.collection("books")
+                    .get()
+                    .addOnSuccessListener { task ->
+                        val booksList = task.toObjects(Book::class.java).map {
+                            if (idsList.contains(it.id)) {
+                                it.copy(isFavorite = true)
+                            } else {
+                                it
+                            }
+                        }
+
+                        onBooks(booksList)
+                    }
+                    .addOnFailureListener {  }
+            }
+        }
+
+    }
+
+    private fun getAllBooksFromCategory(
+        category: String,
+        onBooks: (List<Book>) -> Unit
+    ) {
+        getAllFavsIds { idsList->
+            db.collection("books")
+                .whereEqualTo("category", category)
+                .get()
+                .addOnSuccessListener { task ->
+                    val booksList = task.toObjects(Book::class.java).map {
+                        if (idsList.contains(it.id)) {
+                            it.copy(isFavorite = true)
+                        } else {
+                            it
+                        }
+                    }
+
+                    onBooks(booksList)
+                }
+                .addOnFailureListener {  }
+        }
+
+    }
+
+    private fun onFavs(
+        favorite: Favorite,
+        isFav: Boolean
+    ) {
+        val favsDocRef = getFavsCategoryReference()
+            .document(favorite.id)
+
+        if (isFav) {
+            favsDocRef
+                .set(favorite)
+                .addOnSuccessListener { }
+                .addOnFailureListener { }
+        } else {
+            favsDocRef
+                .delete()
+                .addOnSuccessListener { }
+                .addOnFailureListener { }
+        }
+
+    }
+
+    private fun getFavsCategoryReference(): CollectionReference {
+        return db.collection("users")
+            .document(auth.uid ?: "")
+            .collection("favorites")
+    }
+}
